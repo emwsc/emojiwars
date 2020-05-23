6fd85677430af34a33761ffa90e840ef26654701
Uriy <eatmewhilesocold@gmail.com>
2020-05-23 12:55:01 +0300

    Added typescipt support and import from shared folder into web app

---
+++ web/next.config.js
@@ -0,0 +1,17 @@
+const path = require("path");
+
+module.exports = {
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.module.rules.push({
+      test: /\.ts?$/,
+      use: [
+        defaultLoaders.babel,
+        {
+          loader: "ts-loader",
+        },
+      ],
+      include: [path.join(__dirname, "..", "shared")],
+    });
+    return config;
+  },
+};
---
+++ web/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { convertEmojiToHex } from "../../shared/logic";
+
+export default function Home() {
+  return (
+    <div>
+      <main>😀 = {convertEmojiToHex("😀")}</main>
+    </div>
+  );
+}
