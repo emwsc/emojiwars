7c578bf7a7b68e9aa958f00962298db78e3cc680
Uriy <eatmewhilesocold@gmail.com>
2020-05-24 17:00:06 +0300

    Shuffle keyboard on new game

--- src/shared/components/Keyboard/index.tsx
+++ src/shared/components/Keyboard/index.tsx
@@ -1,7 +1,13 @@
-import React, { useCallback } from "react";
+import React, { useMemo } from "react";
 
 import { KeyboardProps } from "./types";
 
+
+const shuffleArray = (array: any[]) => {
+  return [...array.sort(() => Math.random() - 0.5)];
+}
+
 export const Keyboard = ({ emojis, render, onClick }: KeyboardProps) => {
-  return render({ emojis, onClick });
+  const shufflesEmojis = useMemo(()=> shuffleArray(emojis), [emojis.length]);
+  return render({ emojis: shufflesEmojis, onClick });
 };
